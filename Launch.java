f546aff4d2ff4df211244b10f7542cec55ebff91
shuvajit_103-admin <shuvajitpal103@gmail.com>
2025-08-01 13:40:52 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -1,5 +1,10 @@
 Class Launch{
 	public static void main(String[] args){
-		System.out.println("Exploring Git & GitHub!);
+		System.out.println("Exploring Git & GitHub!"+fun());
+	
+	}
+	public static void fun(){
+		int a = 5,b=10;
+		System.out.println(a+b);
 	}
 }
